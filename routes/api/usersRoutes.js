b597880c79e4b459a4cf64eac56f0c8d76c8ca0b
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-02-22 20:43:25 -0600

    created userControolers file and added updateUser in route

---
+++ routes/api/usersRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+
+
+
+  addAssignment,
+  removeAssignment,
+
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/students/:studentId/assignments
+router.route('/:studentId/assignments').post(addAssignment);
+
+// /api/students/:studentId/assignments/:assignmentId
+router.route('/:studentId/assignments/:assignmentId').delete(removeAssignment);
+
+
+
+
+module.exports = router;
